3cea8f1ce8afa838fbe775a28e06d032bdc5ec23
james <jamesstrine@gmail.com>
2026-04-16 15:16:36 -0400

    Sleep

--- soccerapp/src/main/java/com/zipcode/soccerapp/SoccerappApplication.java
+++ soccerapp/src/main/java/com/zipcode/soccerapp/SoccerappApplication.java
@@ -1,12 +1,22 @@
 package com.zipcode.soccerapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SoccerappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SoccerappApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner openBrowser() {
+		return args -> {
+			Thread.sleep(5000); // give the server a moment to start
+			Runtime.getRuntime().exec(new String[]{"open", "http://localhost:8080"});
+		};
 	}
 }
